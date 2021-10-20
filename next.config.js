06982f75e7397edcdb7fc45040091251eef6f2f6
GiorgiKalatozishvili <giorgi.kalatozishvili.1@btu.edu.ge>
2021-10-20 14:13:10 +0400

    finnally

--- next.config.js
+++ next.config.js
@@ -2,4 +2,8 @@ module.exports = {
   images: {
     domains: ["links.papareact.com"],
   },
+  env: {
+    mapbox_key:
+      "pk.eyJ1IjoiZ2lvcmdpa2FsYXRvemkiLCJhIjoiY2t1ejhsOWJmMnc5dTJvcXZ2NjRibHJlMSJ9.D74m5ejeLIF66usWberyFw",
+  },
 };
